303409bef4e347c93a4ba49874752a4e3e30105a
Gunel04 <alkhasovagunel@gmail.com>
2025-05-02 17:21:48 +0400

    kidupia-app

--- src/shopping/MoreDetails.jsx
+++ src/shopping/MoreDetails.jsx
@@ -21,7 +21,7 @@ const MoreDetails = () => {
                         <img src={singleInfo.image} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width={700} height={500} loading="lazy" />
                     </div>
                     <div className="col-lg-6">
-                        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3"><StaticLang en={singleInfo.nameEn} az={singleInfo.nameAz}/></h1>
+                        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3 detail-title"><StaticLang en={singleInfo.nameEn} az={singleInfo.nameAz}/></h1>
                         <p className="lead"><StaticLang en={singleInfo.descriptionEn} az={singleInfo.descriptionAz}/></p>
                         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
                             <button onClick={()=>{window.history.back()}} type="button" className="btn btn-lg px-4 me-md-2 back-btn"><StaticLang en="Back" az="Geriyə"/></button>
